cc99d3247be4e807b4417b12e1c000bb6f41554c
Arthur Villar <arthur@iMac.local>
2022-02-03 10:38:32 -0500

    Update

--- src/PrimerServiceProvider.php
+++ src/PrimerServiceProvider.php
@@ -20,18 +20,11 @@ class PrimerServiceProvider extends ServiceProvider
         UiCommand::macro('primer-ui', function($command) {
             (new Preset)->install();
 
-            $table = new Table($this->output);
-
-            $table->setRows([
-                ['php artisan ui primer-ui', 'Install the ui package'],
-                ['npm install & npm run dev', 'Install and compile assets'],
-                ['"files": ["support/helpers.php"]', 'Append this to the autoload section in composer.json'],
-                ['composer dumpautoload', 'Refresh composer classes'],
-            ]);
-
             $this->info('All finished! Here are the next steps:');
-
-            $table->render();
+            $this->info('php artisan ui primer-ui -> Install the ui package');
+            $this->info('npm install & npm run dev -> Install and compile assets');
+            $this->info('"files": ["support/helpers.php"] -> Append this to the autoload section in composer.json');
+            $this->info('composer dumpautoload -> Refresh composer classes');
         });
     }
 }
