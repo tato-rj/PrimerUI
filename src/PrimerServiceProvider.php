848239d8e959502c0cf18e665b3c6104f5a09906
Arthur Villar <arthur@iMac.local>
2022-02-03 10:32:02 -0500

    Style info

--- src/PrimerServiceProvider.php
+++ src/PrimerServiceProvider.php
@@ -5,6 +5,8 @@ namespace PrimerUI;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Ui\UiCommand;
 use PrimerUI\Preset;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+use Symfony\Component\Console\Helper\Table;
 
 class PrimerServiceProvider extends ServiceProvider
 {
@@ -15,10 +17,21 @@ class PrimerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        UiCommand::macro('primer', function($command) {
+        UiCommand::macro('primer-ui', function($command) {
             (new Preset)->install();
 
-            $command->info('All finished! Just install and compile the assets and you\'re set to go.');
+            $table = new Table($this->output);
+
+            $table->setRows([
+                ['php artisan ui primer-ui', 'Install the ui package'],
+                ['npm install & npm run dev', 'Install and compile assets'],
+                ['"files": ["support/helpers.php"]', 'Append this to the autoload section in composer.json'],
+                ['composer dumpautoload', 'Refresh composer classes'],
+            ]);
+
+            $this->info('All finished! Here are the next steps:');
+
+            $table->render();
         });
     }
 }
--- src/stubs/BladeServiceProvider.php
+++ src/stubs/BladeServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace PrimerUI;
+namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
