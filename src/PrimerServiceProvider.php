60a33c2065bc8c3973f29729a7f57757c98aa4ce
Arthur Villar <arthur@iMac.local>
2022-02-03 11:19:02 -0500

    Update

--- src/PrimerServiceProvider.php
+++ src/PrimerServiceProvider.php
@@ -21,13 +21,13 @@ class PrimerServiceProvider extends ServiceProvider
             $command->info('PrimerUI scaffolding installed successfully.');
             $command->comment('Here\'s what you\'ll need to do next:');
             
-            $command->table(
+            $command->table([
                 ['npm install && npm run dev', 'Compile your fresh scaffolding'],
                 ['"files": ["support/helpers.php"]', 'Copy this in the autoload section in composer.json'],
                 ['composer dumpautoload', 'Refresh composer classes'],
                 ['App\Providers\BladeServiceProvider::class,', 'Copy this to the list of service providers'],
                 ['php artisan view:clear', 'Update view files to enable custom components'],
-            );
+            ]);
         });
     }
 }
