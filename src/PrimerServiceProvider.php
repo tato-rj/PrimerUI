3f56c841480f9dff3cc24ef64fd4253d0bb80ceb
Arthur Villar <arthur@iMac.local>
2022-02-03 10:58:08 -0500

    Update

--- src/PrimerServiceProvider.php
+++ src/PrimerServiceProvider.php
@@ -5,8 +5,6 @@ namespace PrimerUI;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Ui\UiCommand;
 use PrimerUI\Preset;
-use Symfony\Component\Console\Formatter\OutputFormatterStyle;
-use Symfony\Component\Console\Helper\Table;
 
 class PrimerServiceProvider extends ServiceProvider
 {
@@ -20,11 +18,33 @@ class PrimerServiceProvider extends ServiceProvider
         UiCommand::macro('primer-ui', function($command) {
             (new Preset)->install();
 
-            $command->info('All finished! Here are the next steps:');
-            $command->info('php artisan ui primer-ui -> Install the ui package');
-            $command->info('npm install & npm run dev -> Install and compile assets');
-            $command->info('"files": ["support/helpers.php"] -> Append this to the autoload section in composer.json');
-            $command->info('composer dumpautoload -> Refresh composer classes');
+            $this->info('PrimerUI scaffolding installed successfully.');
+            $this->comment('Here\'s what you\'ll need to do next:');
+            
+            $this->newLine();
+
+            $this->warn('npm install && npm run dev');
+            $this->comment('Compile your fresh scaffolding');
+
+            $this->newLine();
+
+            $this->warn('"files": ["support/helpers.php"]');
+            $this->comment('Copy this in the autoload section in composer.json.');
+
+            $this->newLine();
+
+            $this->warn('composer dumpautoload');
+            $this->comment('Refresh composer classes.');
+
+            $this->newLine();
+
+            $this->warn('App\Providers\BladeServiceProvider::class,');
+            $this->comment('Copy this to the list of service providers.');
+
+            $this->newLine();
+
+            $this->warn('php artisan view:clear');
+            $this->comment('Update view files to enable custom components.');
         });
     }
 }
